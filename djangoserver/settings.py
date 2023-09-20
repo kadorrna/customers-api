d4ba4a41d8fe347cf7a446284574f98354dc4186
Kado <juan.alba@gmail.com>
2023-09-19 22:08:28 -0300

    Updating configs

--- djangoserver/settings.py
+++ djangoserver/settings.py
@@ -83,15 +83,13 @@ WSGI_APPLICATION = 'djangoserver.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': os.getenv('DB_NAME'),
         'USER': os.getenv('DB_USER'),
-        'HOST': os.getenv('DATABASE_URL')
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'PORT': os.getenv('DB_PORT'),
+        'HOST': os.getenv('DATABASE_URL'),
     }
 }
 
